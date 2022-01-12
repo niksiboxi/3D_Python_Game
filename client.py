88488c9961f252b51e0db01a7ba583bade2f7ba3
niksiboxi <niksiboxi@hotmail.com>
2022-01-12 20:28:50 +0200

    Initial client

---
+++ client.py
@@ -0,0 +1,32 @@
+import pygame
+import sys
+import os
+sys.path.insert(0, './pvp_raycast')
+from game import Game
+import constants as c
+import socket
+import time
+import pickle
+from sprite import Sprite
+import threading 
+
+pygame.init()
+
+pygame.init()
+
+# Set up the drawing window
+screen = pygame.display.set_mode((c.SCREEN_WIDTH,c.SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+game = Game()
+
+pygame.mouse.set_visible(False)
+pygame.event.set_grab(True)
+
+# Main loop
+while not game.done:
+    events = pygame.event.get()
+    game.draw(screen)
+    game.input_handle()
+    pygame.display.flip()
+    clock.tick()
+pygame.quit()
